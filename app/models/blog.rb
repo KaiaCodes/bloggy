40267f073eefcea3061e7db92aeef883873198ad
Kaia Alderson <kaiawrites@yahoo.com>
2013-11-25 21:33:43 -0500

    added tags

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,13 @@
 class Blog < ActiveRecord::Base
 
 	has_many :comments
-		validates :title, length: {minimum: 5}
-		validates :content, length: {minimum: 2}
+	has_many :blog_tags
+	has_many :tags, :through => :blog_tags
+
+		validates :title, length: {minimum: 2}
+		validates :content, length: {minimum: 10}
+
+
+		
+		
 end
---
+++ app/models/tag.rb
@@ -0,0 +1,7 @@
+class Tag < ActiveRecord::Base
+	has_many :blog_tags
+	has_many :blogs, :through => :blog_tags
+
+	validates :name, length: {minimum: 1}
+
+end
